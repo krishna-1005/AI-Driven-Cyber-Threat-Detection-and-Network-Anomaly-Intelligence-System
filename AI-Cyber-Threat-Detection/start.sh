0ba943a86a69d2fc3113e924594c6e32cf81a18b
krishna-1005 <krishkulkarni1005@gmail.com>
2026-03-16 23:03:27 +0530

    Fix start script

--- AI-Cyber-Threat-Detection/start.sh
+++ AI-Cyber-Threat-Detection/start.sh
@@ -1,2 +1,7 @@
-cd AI-Cyber-Threat-Detection
-gunicorn backend.app:app --bind 0.0.0.0:$PORT
+#!/bin/bash
+
+echo "Starting Flask API..."
+python AI-Cyber-Threat-Detection/backend/app.py &
+
+echo "Starting Streamlit Dashboard..."
+streamlit run AI-Cyber-Threat-Detection/dashboard/dashboard.py --server.port 10000 --server.address 0.0.0.0
