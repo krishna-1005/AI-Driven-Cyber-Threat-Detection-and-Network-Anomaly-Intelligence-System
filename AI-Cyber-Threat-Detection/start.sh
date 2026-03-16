e5e7f88c097595e6e9b59d87fe04226d8241deb9
krishna-1005 <krishkulkarni1005@gmail.com>
2026-03-17 02:48:23 +0530

    redv

--- AI-Cyber-Threat-Detection/start.sh
+++ AI-Cyber-Threat-Detection/start.sh
@@ -3,5 +3,8 @@
 echo "Starting Flask API..."
 python AI-Cyber-Threat-Detection/backend/app.py &
 
+echo "Starting Traffic Simulator..."
+python AI-Cyber-Threat-Detection/simulator/send_data.py &
+
 echo "Starting Streamlit Dashboard..."
 streamlit run AI-Cyber-Threat-Detection/dashboard/dashboard.py --server.port 10000 --server.address 0.0.0.0
